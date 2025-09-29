224abc5417decda01095a6f3c60dea6df83b80b4
Nikhil Thakur <nikhilthakur8012004@gmail.com>
2025-09-29 19:21:04 +0530

    feat: refactor handleGetRankingPage to use async/await for database queries and improve type definitions

--- src/app/ranking/[pageNo]/action.ts
+++ src/app/ranking/[pageNo]/action.ts
@@ -1,14 +1,20 @@
 import User from "@/models/leetcodeData";
 
 import connectDB from "@/utils/db";
-
+type User = {
+	_id: string;
+	username: string;
+	ranking: number;
+	countryName?: string;
+	userAvatar?: string;
+};
 async function handleGetRankingPage(pageNo: number) {
 	const page = pageNo || 1;
 	const limit = 25;
 	const skip = (page - 1) * limit;
 	try {
 		await connectDB();
-		const users = await User.find({
+		const users = User.find({
 			ranking: {
 				$exists: true,
 			},
@@ -17,11 +23,15 @@ async function handleGetRankingPage(pageNo: number) {
 			.skip(skip)
 			.limit(limit);
 
-		const totalUsers = await User.countDocuments({
+		const totalUsers = User.countDocuments({
 			ranking: { $exists: true },
 		});
-		const totalPages = Math.ceil(totalUsers / limit);
-		return { users, totalPages };
+
+		const [usersResult, totalUsersResult]: [User[], number] =
+			await Promise.all([users, totalUsers]);
+		const totalPages = Math.ceil(totalUsersResult / limit);
+
+		return { users: usersResult, totalPages };
 	} catch (error) {
 		console.error("Error fetching ranking page:", error);
 	}

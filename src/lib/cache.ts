59b2bdea52dcf9154da47e1c4ebdf838c56a4e5f
Nikhil Thakur <nikhilthakur8012004@gmail.com>
2025-10-07 02:29:53 +0530

    fix: add eslint disable comment for any type in setCache function

--- src/lib/cache.ts
+++ src/lib/cache.ts
@@ -1,5 +1,6 @@
 import redis from "@/utils/redis";
 
+// eslint-disable-next-line @typescript-eslint/no-explicit-any
 export const setCache = async (key: string, value: any, ttl = 300) => {
 	await redis.set(key, JSON.stringify(value), "EX", ttl);
 };

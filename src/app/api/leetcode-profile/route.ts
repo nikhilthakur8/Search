18145403593ee0fd4d835ca6e99dd3d37e85c15e
Nikhil Thakur <nikhilthakur8012004@gmail.com>
2025-10-12 06:46:45 +0530

    refactor: streamline profile fetching logic in POST request by removing redundant function

--- src/app/api/leetcode-profile/route.ts
+++ src/app/api/leetcode-profile/route.ts
@@ -3,109 +3,74 @@ import connectDB from "@/utils/db";
 import axios from "axios";
 import { NextRequest, NextResponse } from "next/server";
 
-async function handleGetLeetCodeProfileAndSave(username: string) {
-	
-	const query = `
-    query userPublicProfile($username: String!) {
-      matchedUser(username: $username) {
-        username
-        githubUrl
-        twitterUrl
-        linkedinUrl
-        profile {
-          ranking
-          userAvatar
-          realName
-          aboutMe
-          school
-          websites
-          countryName
-          company
-          jobTitle
-        }
-      }
-    }
-  `;
-	const variables = { username };
-
-	const response = await axios.post("https://leetcode.com/graphql/", {
-		query,
-		variables,
-	});
-
-	const matchedUser = response.data.data.matchedUser;
-	if (!matchedUser) return null;
-
-	const data = {
-		username: matchedUser.username,
-		githubUrl: matchedUser.githubUrl,
-		twitterUrl: matchedUser.twitterUrl,
-		linkedinUrl: matchedUser.linkedinUrl,
-		...matchedUser.profile,
-	};
-
-	const updatedUser = await User.findOneAndUpdate(
-		{ username },
-		{ $set: data },
-		{
-			new: true,
-			upsert: true,
-			collation: { locale: "en", strength: 2 },
-		}
-	);
-
-	return updatedUser;
-}
-
 export async function POST(request: NextRequest) {
 	try {
 		await connectDB();
 
 		const { username } = await request.json();
-
-		// Find in Database (case-insensitive)
-		let userData = await User.findOne({ username }).collation({
-			locale: "en",
-			strength: 2,
+		const query = `
+    query userPublicProfile($username: String!) {
+  matchedUser(username: $username) {
+    username
+    githubUrl
+    twitterUrl
+    linkedinUrl
+    profile {
+      ranking
+      userAvatar
+      realName
+      aboutMe
+      school
+      websites
+      countryName
+      company
+      jobTitle
+    }
+  }
+}
+    `;
+		const variables = { username };
+		const response = await axios.post("https://leetcode.com/graphql/", {
+			query,
+			variables,
 		});
-
-		// if Found in DB, return and do fire and forget to update in background
-		if (userData) {
-
-			handleGetLeetCodeProfileAndSave(username);
-
+		if (!response.data.data.matchedUser) {
 			return NextResponse.json(
-				{
-					message: "Profile data fetched successfully (from cache)",
-					profile: userData,
-				},
-				{ status: 200 }
-			);
-
-		} else {
-
-			// else fetch from leetcode, store in DB and return
-			userData = await handleGetLeetCodeProfileAndSave(username);
-
-			if (!userData) {
-				return NextResponse.json(
-					{ error: "username might have changed or deleted" },
-					{ status: 404 }
-				);
-			}
-
-			return NextResponse.json(
-				{
-					message: "Profile data fetched and stored successfully",
-					profile: userData,
-				},
-				{ status: 200 }
+				{ error: "User not found" },
+				{ status: 404 }
 			);
 		}
+		const data = {
+			username: response.data.data.matchedUser.username,
+			githubUrl: response.data.data.matchedUser.githubUrl,
+			twitterUrl: response.data.data.matchedUser.twitterUrl,
+			linkedinUrl: response.data.data.matchedUser.linkedinUrl,
+			...response.data.data.matchedUser.profile,
+		};
+
+		await User.findOneAndUpdate(
+			{ username },
+			{ $set: data },
+			{
+				new: true,
+				upsert: true,
+				collation: {
+					locale: "en",
+					strength: 2,
+				},
+			}
+		);
+		return NextResponse.json(
+			{
+				message: "Profile data fetched and stored successfully",
+				profile: data,
+			},
+			{
+				status: 200,
+			}
+		);
 	} catch (error) {
-
 		console.error("Error fetching profile data:", error);
-
 		return NextResponse.json(
 			{ error: "Failed to fetch profile data" },
 			{ status: 500 }

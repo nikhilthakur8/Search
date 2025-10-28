b1f3fbe08bd643eaf06bdb9e17301d348a80b349
Nikhil Thakur <nikhilthakur8012004@gmail.com>
2025-10-29 04:03:27 +0530

    refactor: replace countDocuments with estimatedDocumentCount for performance improvement

--- src/app/api/stats/route.ts
+++ src/app/api/stats/route.ts
@@ -5,7 +5,7 @@ import { NextResponse } from "next/server";
 export async function GET() {
 	try {
 		await connectDB();
-		const count = await User.countDocuments();
+		const count = await User.estimatedDocumentCount();
 		const indexingRatePerHour = await User.find({
 			$and: [
 				{
